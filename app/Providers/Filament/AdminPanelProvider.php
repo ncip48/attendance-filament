0c495c67262c10355d85fd8165f906bda7610f92
ncip48 <mbahcip00@gmail.com>
2023-11-07 16:16:35 +0700

    prefix

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('app')
             ->login()
             // ->navigationItems([
             //     NavigationItem::make('Karyawan', Pages\ListRecords::class)
